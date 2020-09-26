082f9fea3b9233cfff1209e1673d2a5a080aafc6
Joao Lucas <joao.lucas.ssr@gmail.com>
2020-09-26 17:56:16 -0300

    [feature] adding icons to the sidebar

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,20 +1,55 @@
 import React from 'react'
-import * as MdIcons from 'react-icons/md';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
+import { MdFeedback } from 'react-icons/md';
+import { VscGear } from 'react-icons/vsc';
+import { BiSupport } from 'react-icons/bi';
+import { BiExit } from 'react-icons/bi';
+import { BsClipboardData } from 'react-icons/bs';
+import { HiDocumentReport } from 'react-icons/hi';
+import { GoBug } from 'react-icons/go';
 
 export const SidebarData = [
     {
-        title: ' Home',
+        title: ' Indicadores',
         path: '/',
-        icon: <AiIcons.AiFillHome />,
+        icon: <HiDocumentReport />,
+        cName: 'nav-text'
+    },
+    {
+        title: ' Sistemas',
+        path: '/',
+        icon: <VscGear />,
+        cName: 'nav-text'
+    },
+    {
+        title: ' Suporte',
+        path: '/support',
+        icon: <BiSupport/>,
+        cName: 'nav-text'
+    },
+    {
+        title: ' Avaliar Safra',
+        path: '/',
+        icon: <BsClipboardData />,
         cName: 'nav-text'
     },
     {
         title: ' Feedback',
         path: '/feedback',
-        icon: <MdIcons.MdFeedback />,
+        icon: <MdFeedback />,
         cName: 'nav-text'
     },
+    {
+        title: ' Pragas',
+        path: '/',
+        icon: <GoBug />,
+        cName: 'nav-text'
+    },
+    {
+        title: ' Sair',
+        path: '/',
+        icon: <BiExit />,
+        cName: 'nav-text'
+    },
+
 
 ]
