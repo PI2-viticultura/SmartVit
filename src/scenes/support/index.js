dae9a01013e9666586093d010ad674dba51d2762
Lucas362 <lucasvitorifg@gmail.com>
2020-09-25 21:44:03 -0300

    [Feature] Adding service request
    Co-authored-by: Adrianne <driah_as@hotmail.com>

--- src/scenes/support/index.js
+++ src/scenes/support/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import api from '../../services/api';
 
 function Support() {
+    const [problem, setProblem] = useState('');
+    const [priority, setPriority] = useState('');
+    const [description, setDescription] = useState('');
+
+    const makeRequest = async () => {
+        try {
+            const response = await api.post('/support', {
+                problem: 'outros',
+                priority: 'alta',
+                description: 'description',
+            },
+            {
+                "Content-Type": "application/json",
+                "X-Requested-With": "XMLHttpRequest"
+            });
+            console.log(response);
+
+        } catch (_err) {
+            console.log(_err);
+        }
+    }
     return (
         <div>
-            <h3>Drih é linda</h3>
+            <button onClick={() => makeRequest()}>Hora do show</button>
         </div>
     );
-  }
+}
   
   export default Support;
   
