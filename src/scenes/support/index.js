777f5fd8604b870f361f0608c2813daf4e5f55ae
Lucas362 <lucasvitorifg@gmail.com>
2020-09-25 22:16:13 -0300

    [Feature] Creating form nd sending data to backend
    Co-authored-by: Adrianne <driah_as@hotmail.com>

--- src/scenes/support/index.js
+++ src/scenes/support/index.js
@@ -3,30 +3,58 @@ import React, { useState } from 'react';
 import api from '../../services/api';
 
 function Support() {
-    const [problem, setProblem] = useState('');
-    const [priority, setPriority] = useState('');
+    const [problem, setProblem] = useState('Sistema eletrônico com defeito');
+    const [priority, setPriority] = useState('alta');
     const [description, setDescription] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
     const makeRequest = async () => {
-        try {
-            const response = await api.post('/support', {
-                problem: 'outros',
-                priority: 'alta',
-                description: 'description',
+            await api.post('/support', {
+                problem,
+                priority,
+                description,
             },
             {
                 "Content-Type": "application/json",
                 "X-Requested-With": "XMLHttpRequest"
-            });
-            console.log(response);
+            }).then(res => {
+                setError('');
+                setSuccess('success');
+            }).catch(error => {
+                setError('error');
+                setSuccess('');
+            })
 
-        } catch (_err) {
-            console.log(_err);
-        }
     }
     return (
         <div>
-            <button onClick={() => makeRequest()}>Hora do show</button>
+            {
+                error !== '' && <p>Aconteceu um erro na sua requisição, por favor cheque os dados enviados</p>
+            }
+            {
+                success !== '' && <p>Em breve você receberá uma ligação de um dos nossos atendentes</p>
+            }
+            <p>Problema:</p>
+            <select onChange={e => {setProblem(e.target.value)}}>
+                <option value="Sistema eletrônico com defeito">Sistema eletrônico com defeito</option>
+                <option value="Problema do software">Problema do software</option>
+                <option value="Dados incoerentes">Dados incoerentes</option>
+                <option value="Perfil de acesso">Perfil de acesso</option>
+                <option value="Sobre o contrato">Sobre o contrato</option>
+                <option value="outros">Outros</option>
+            </select>
+
+            <p>Classificação:</p>
+            <select onChange={e => {setPriority(e.target.value)}}>
+                <option value="alta">Atrapalha meu serviço</option>
+                <option value="média">Estou confuso</option>
+                <option value="baixa">Sugestão</option>
+            </select>
+
+            <p>Descrição:</p>
+            <textarea onChange={e => setDescription(e.target.value)}></textarea>
+            <button onClick={() => makeRequest()}>ENVIAR</button>
         </div>
     );
 }
