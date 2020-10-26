fbf774d8812f4ea804f7db8c5be2a8de969377d3
Luciano Santos <luciano_z7@hotmail.com>
2020-10-26 15:17:31 -0300

    [Feature] Adicionando validações de login e redirecionamento

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import {
   BrowserRouter,
-  Switch,
-  Route
+  Route,
+  Switch
 } from "react-router-dom";
 import Notification from './scenes/notification';
 import Indicator from './scenes/indicator';
@@ -11,31 +11,63 @@ import Feedback from './scenes/feedback';
 import Navbar from './components/Navbar';
 import Support from './scenes/support';
 import Pest from './scenes/pest';
+import Login from './scenes/login';
+import Logout from "./scenes/logout"
 import './App.css';
-import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import { ThemeProvider } from '@chakra-ui/core';
+import { isAuthenticated } from './services/auth';
+
+
+const DefaultContainer = () => {
+  return (
+    <div className="container">
+      <Route path='/support'>
+        <Navbar />
+        <Support />
+      </Route>
+      <Route path='/feedback'>
+        <Navbar />
+        <Feedback />
+      </Route>
+      <Route path='/pest'>
+        <Navbar />
+        <Pest />
+      </Route>
+      <Route path='/indicator'>
+        <Navbar />
+        <Indicator />
+      </Route>
+      <Route path='/notifications'>
+        <Navbar />
+        <Notification />
+      </Route>
+      <Route path="/logout">
+        <Logout />
+      </Route>
+    </div>
+  )
+}
+
+const LoginContainer = () => {
+  return (
+    <div className="container">
+      <Route path="/" component={Login}/>
+      {/* <Route path="/login" component={Login} /> */}
+      <Route path="/logout" component={Logout} />
+    </div>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider>
-        <Navbar />
-          <Switch>
-            <Route path='/support'>
-              <Support />
-            </Route>
-            <Route path='/feedback'>
-              <Feedback />
-          </Route>
-          <Route path='/pest'>
-            <Pest />
-          </Route>
-          <Route path='/indicator'>
-            <Indicator />
-          </Route>
-          <Route path='/notifications'>
-            <Notification />
-          </Route>
-        </Switch>
+        <div className="App">
+          {
+            isAuthenticated() ? <Route component={DefaultContainer} />
+              : <Route component={LoginContainer} />
+          }
+        </div>
       </ThemeProvider>
     </BrowserRouter>
   );
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,17 @@
   
 import axios from 'axios'
+import { getToken } from './auth';
 
 const api =  axios.create({
     baseURL: 'http://localhost:8000',
+});
+
+api.interceptors.request.use(async configure => {
+    const token = getToken();
+    if(token){
+        configure.headers.Authorization = `Bearer ${token}`
+    }
+    return configure;
 });
 
 export default api;
