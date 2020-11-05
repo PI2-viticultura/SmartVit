f1a86f3f5227015310d4f5bda5dafaa5370ee7a2
Lucas362 <lucasvitorifg@gmail.com>
2020-11-05 01:12:43 -0300

    [Feature] Adding multiple graphs

--- src/scenes/indicator/index.js
+++ src/scenes/indicator/index.js
@@ -8,47 +8,55 @@ import GaugeChart from 'react-gauge-chart';
 
 
 function Indicator(){
-    let chartRef = React.createRef();
+    let refs = [React.createRef(), React.createRef(), React.createRef()]
     let qualityChartRef = React.createRef();
     const [fuzzy, setFuzzy] = useState([]);
     const [showGraph, setShowGraph] = useState(-1);
     let var_fuzzy = [];
+    var date = "05/10/2020";
+    var dateParts = date.split("/");
+    var today = new Date().getTime();
+    var dateInDatetime = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]).getTime();
+    var msDiff = today - dateInDatetime;    //Future date - current date
+    var days = Math.floor(msDiff / (1000 * 60 * 60 * 24));
 
     React.useEffect(() => {
-        const myChartRef = chartRef.current.getContext("2d");
-        
-        new Chart(myChartRef, {
-            type: "line",
-            data: {
-                //Bring in data
-                labels: ["Jan", "Feb", "March", "May"],
-                datasets: [
-                    {
-                        label: "pH",
-                        data: [7, 5, 0, 15],
-                        pointBackgroundColor: "#fff",
-                        borderColor: "#7597C5",
-                        backgroundColor: "#7496C4"
-                    },
-                    {
-                        label: "Temperatura",
-                        data: [25, 26, 32, 18],
-                        pointBackgroundColor: "#fff",
-                        borderColor: "#F7AB79",
-                        backgroundColor: "#F7AB79"
-                    },
-                    {
-                        label: "Umidade",
-                        data: [60, 55, 38, 25],
-                        pointBackgroundColor: "#fff",
-                        borderColor: "#ABDFFF",
-                        backgroundColor: "#ABDFFF"
-                    }
-                ]
-            },
-            options: {
-                //Customize chart options
-            }
+        refs.map(chartRef => {
+            const myChartRef = chartRef.current.getContext("2d");
+            
+            new Chart(myChartRef, {
+                type: "line",
+                data: {
+                    //Bring in data
+                    labels: ["Jan", "Feb", "March", "May"],
+                    datasets: [
+                        {
+                            label: "pH",
+                            data: [7, 5, 0, 15],
+                            pointBackgroundColor: "#fff",
+                            borderColor: "#7597C5",
+                            backgroundColor: "#7496C4"
+                        },
+                        {
+                            label: "Temperatura",
+                            data: [25, 26, 32, 18],
+                            pointBackgroundColor: "#fff",
+                            borderColor: "#F7AB79",
+                            backgroundColor: "#F7AB79"
+                        },
+                        {
+                            label: "Umidade",
+                            data: [60, 55, 38, 25],
+                            pointBackgroundColor: "#fff",
+                            borderColor: "#ABDFFF",
+                            backgroundColor: "#ABDFFF"
+                        }
+                    ]
+                },
+                options: {
+                    //Customize chart options
+                }
+            });
         });
     }, []);
 
@@ -169,7 +177,7 @@ function Indicator(){
             </div>
             
             <div>
-                <Stepper days={10}/>
+                <Stepper days={days}/>
             </div>
             <div className="quality-main">
                 <div className="quality-title">
@@ -196,30 +204,34 @@ function Indicator(){
                     </div>
                 </div>
             </div>
-            <div className="system-graph-container">
-                <div className="system-graph-item" onClick={() => { showGraph == 1 ? setShowGraph(-1) : setShowGraph(1)}}>
-                    Sistema X    7.0 pH - 40% umidade - 34ºC temperaura
-                    {showGraph == 1 ?
-                    <img
-                        height={12}
-                        alt="..."
-                        src={require("../../imgs/pin-icon.png")}
-                    />
-                    :
-                    <img
-                        height={12}
-                        alt="..."
-                        src={require("../../imgs/inverted-pin-icon.png")}
-                    />
-                    }
-                </div>
-                <div className="graph" style={showGraph == 1 ? {display: "block"} : {display: "none"}}>
-                    <canvas
-                        id="myChart"
-                        ref={chartRef}
-                    />
-                </div>
-            </div>
+            {refs.map((chartRef, i) => {
+                console.log(i)
+                return (<div key={i} className="system-graph-container">
+                    <div className="system-graph-item" onClick={() => { showGraph == i ? setShowGraph(-1) : setShowGraph(i)}}>
+                        Sistema X    7.0 pH - 40% umidade - 34ºC temperaura
+                        {showGraph == 1 ?
+                        <img
+                            height={12}
+                            alt="..."
+                            src={require("../../imgs/pin-icon.png")}
+                        />
+                        :
+                        <img
+                            height={12}
+                            alt="..."
+                            src={require("../../imgs/inverted-pin-icon.png")}
+                        />
+                        }
+                    </div>
+                    <div className="graph" style={showGraph == i ? {display: "block"} : {display: "none"}}>
+                        <canvas
+                            id={i}
+                            ref={chartRef}
+                        />
+                    </div>
+                </div>)
+            })
+            }
         </div>
     
     )
