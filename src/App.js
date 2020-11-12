6bb6251cb46519951b4bbfa9c66e787cd6ee9b8d
Luciano Santos <luciano_z7@hotmail.com>
2020-11-11 22:25:58 -0300

    hereee we go

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Notification from "./scenes/notification";
-import Indicator from "./scenes/indicator";
-import Feedback from "./scenes/feedback";
-import Navbar from "./components/Navbar";
-import Support from "./scenes/support";
-import Pest from "./scenes/pest";
-import Login from "./scenes/login";
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from "react-router-dom";
+import Notification from './scenes/notification';
+import Indicator from './scenes/indicator';
+import Feedback from './scenes/feedback';
+import Navbar from './components/Navbar';
+import Support from './scenes/support';
+import Pest from './scenes/pest';
+import Login from './scenes/login';
 import Logout from "./scenes/logout";
 import System from "./scenes/system";
-import "./App.css";
-import { ThemeProvider } from "@chakra-ui/core";
-import { isAuthenticated } from "./services/auth";
+import './App.css';
+import { ThemeProvider } from '@chakra-ui/core';
+import { isAuthenticated } from './services/auth';
+
 
 const DefaultContainer = () => {
   return (
     <div className="container">
-      <Route path="/support">
+      <Route path='/support'>
         <Navbar />
         <Support />
       </Route>
-      <Route path="/feedback">
+      <Route path='/feedback'>
         <Navbar />
         <Feedback />
       </Route>
-      <Route path="/pest">
+      <Route path='/pest'>
         <Navbar />
         <Pest />
       </Route>
-      <Route path="/indicator">
+      <Route path='/indicator'>
         <Navbar />
         <Indicator />
       </Route>
-      <Route path="/notifications">
+      <Route path='/notifications'>
         <Navbar />
         <Notification />
       </Route>
@@ -44,49 +49,29 @@ const DefaultContainer = () => {
         <Logout />
       </Route>
     </div>
-  );
-};
+  )
+}
 
 const LoginContainer = () => {
   return (
     <div className="container">
-      <Route path="/" component={Login} />
+      <Route path="/" component={Login}/>
       {/* <Route path="/login" component={Login} /> */}
       <Route path="/logout" component={Logout} />
     </div>
-  );
-};
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider>
-        <Route path="/support">
-          <Navbar />
-          <Support />
-        </Route>
-        <Route path="/feedback">
-          <Navbar />
-          <Feedback />
-        </Route>
-        <Route path="/pest">
-          <Navbar />
-          <Pest />
-        </Route>
-        <Route path="/indicator">
-          <Navbar />
-          <Indicator />
-        </Route>
-        <Route path="/notifications">
-          <Navbar />
-          <Notification />
-        </Route>
-        <Route path="/system">
-          <System />
-        </Route>
-        <Route path="/logout">
-          <Logout />
-        </Route>
+        <div className="App">
+          {
+            isAuthenticated() ? <Route component={DefaultContainer} />
+              : <Route component={LoginContainer} />
+          }
+        </div>
       </ThemeProvider>
     </BrowserRouter>
   );
